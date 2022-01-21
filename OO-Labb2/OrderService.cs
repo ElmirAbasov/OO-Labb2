9977d9ec0076a2a0e03fe0183a9d3c1fa4218060
Elmir Abasov <56082840+Xizafren@users.noreply.github.com>
2022-01-21 12:02:41 +0100

    Cleanup

--- OO-Labb2/OrderService.cs
+++ OO-Labb2/OrderService.cs
@@ -1,12 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
+﻿
 namespace OO_Labb2
 {
-    
+
     public class OrderService
     {
         private IConsoleLogger _consoleLogger;
